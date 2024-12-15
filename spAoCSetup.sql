7ef2a86cf241391562791fa94b4f864731191325
Gunnar Johansson <46684570+oaklake89@users.noreply.github.com>
2024-12-15 17:14:46 +0100

    Update spAoCSetup.sql

--- spAoCSetup.sql
+++ spAoCSetup.sql
@@ -6,30 +6,40 @@ CREATE PROCEDURE [spAoCSetup]
     @rowTerminator NVARCHAR(12) = '\n'
 AS
 BEGIN
-    DECLARE @filePath NVARCHAR(255) = CONCAT('/var/opt/mssql/aoc/', @year, '/day', RIGHT(CONCAT('0', @day), 2), '/input.txt'),
-            @tableName NVARCHAR(128) = CONCAT('Day', RIGHT(CONCAT('0', @day), 2));
+    BEGIN TRY
+        DECLARE @filePath NVARCHAR(255) = CONCAT('/var/opt/mssql/aoc/', @year, '/day', RIGHT(CONCAT('0', @day), 2), '/input.txt'),
+                @tableName NVARCHAR(128) = CONCAT('Day', RIGHT(CONCAT('0', @day), 2));
 
-    IF NOT EXISTS (
-        SELECT 1
-        FROM [INFORMATION_SCHEMA].[TABLES]
-        WHERE [TABLE_NAME] = @tableName
-    )
-    BEGIN
-        DECLARE @createTableCommand NVARCHAR(MAX) = N'
-            CREATE TABLE ' + QUOTENAME(@tableName) + ' (' + @columnDefinitions + ')';
+        IF NOT EXISTS (
+            SELECT 1
+            FROM [INFORMATION_SCHEMA].[TABLES]
+            WHERE [TABLE_NAME] = @tableName
+        )
+        BEGIN
+            DECLARE @createTableCommand NVARCHAR(MAX) = N'
+                CREATE TABLE ' + QUOTENAME(@tableName) + ' (' + @columnDefinitions + ')';
 
-        EXECUTE [sys].[sp_executesql] @createTableCommand;
-    END
+            EXECUTE [sys].[sp_executesql] @createTableCommand;
 
-    DECLARE @bulkInsertCommand NVARCHAR(MAX) = N'
-        BULK INSERT ' + QUOTENAME(@tableName) + '
-        FROM ''' + @filePath + '''
-        WITH (
-            FIELDTERMINATOR = ''' + @fieldTerminator + ''',
-            ROWTERMINATOR = ''' + @rowTerminator + ''',
-            FIRSTROW = 1,
-            TABLOCK
-        );';
+            PRINT 'Table created: ' + @tableName;
+        END
 
-    EXECUTE [sys].[sp_executesql] @bulkInsertCommand;
+        DECLARE @bulkInsertCommand NVARCHAR(MAX) = N'
+            BULK INSERT ' + QUOTENAME(@tableName) + '
+            FROM ''' + @filePath + '''
+            WITH (
+                FIELDTERMINATOR = ''' + @fieldTerminator + ''',
+                ROWTERMINATOR = ''' + @rowTerminator + ''',
+                FIRSTROW = 1,
+                TABLOCK
+            );';
+
+        EXECUTE [sys].[sp_executesql] @bulkInsertCommand;
+
+        PRINT 'Data is imported from: ' + @filePath;
+    END TRY
+
+    BEGIN CATCH
+        PRINT 'An error occurred: ' + ERROR_MESSAGE();
+    END CATCH
 END;
